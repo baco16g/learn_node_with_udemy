f2a1aa6c7de3f967ea10224e1380fb8b0a61d89b
baco16g <baco.16g@gmail.com>
2018-11-10 23:56:19 +0900

    feat(node-todo-api): add test for GET /users/me and POST /users

---
+++ node-todo-api/server/test/seed/seed.js
@@ -0,0 +1,60 @@
+const { ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+const { Todo } = require('../../models/todo')
+const { User } = require('../../models/user')
+
+const userOneId = new ObjectId()
+const userTwoId = new ObjectId()
+const users = [{
+  _id: userOneId,
+  email: 'abc@example.com',
+  password: 'userOnePass',
+  tokens: [
+    {
+      access: 'auth',
+      token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+    }
+  ]
+}, {
+  _id: userTwoId,
+  email: 'def@example.com',
+  password: 'userTwoPass'
+}]
+
+const todos = [
+  {
+    _id: new ObjectId(),
+    text: 'First test todo'
+  },
+  {
+    _id: new ObjectId(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+  }
+]
+
+const populateTodos = done => {
+  Todo.deleteMany()
+    .then(() => Todo.insertMany(todos))
+    .then(() => done())
+}
+
+const populateUsers = done => {
+  User.deleteMany()
+    .then(() => Promise.all(
+      users.reduce(
+        (acc, user) => [...acc, new User(user).save()],
+        []
+      )
+    ))
+    .then(() => done())
+}
+
+module.exports = {
+  todos,
+  populateTodos,
+  users,
+  populateUsers
+}
--- node-todo-api/server/test/server.test.js
+++ node-todo-api/server/test/server.test.js
@@ -6,24 +6,10 @@ const { app } = require('../server')
 const { Todo } = require('../models/todo')
 const { User } = require('../models/user')
 
-const todos = [
-  {
-    _id: new ObjectId(),
-    text: 'First test todo'
-  },
-  {
-    _id: new ObjectId(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-  }
-]
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed')
 
-beforeEach(done => {
-  Todo.deleteMany()
-    .then(() => Todo.insertMany(todos))
-    .then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
   it('should create a new Todo', done => {
@@ -186,5 +172,76 @@ describe('PATCH /todos/:id', () => {
       .patch('/todos/123abc')
       .expect(404)
       .end(done)
+  })
+})
+
+describe('GET /user/me', () => {
+  it('should return user if authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(res => {
+        expect(res.body._id).toBe(users[0]._id.toHexString())
+        expect(res.body.email).toBe(users[0].email)
+      })
+      .end(done)
+  })
+
+  it('should return 401 if not authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect(res => {
+        expect(res.body).toEqual({})
+      })
+      .end(done)
+  })
+})
+
+describe('POST /users', () => {
+  it('should create a user', done => {
+    const email = 'example@example.com'
+    const password = 'abcd1234'
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toBeDefined()
+        expect(res.body._id).toBeDefined()
+        expect(res.body.email).toBe(email)
+      })
+      .end(err => {
+        if (err) {
+          return done(err)
+        }
+        User.findOne({ email })
+          .then(user => {
+            expect(user).toBeDefined()
+            expect(user.password).not.toBe(password)
+            done()
+          })
+      })
+  })
+
+  it('should return validation errors if request invalid', done => {
+    const email = 'test-string'
+    const password = '1234567890'
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(400)
+      .end(done)
+  })
+
+  it('should not create user if email in use', done => {
+    const email = users[0].email
+    const password = 'samplePassword'
+    request(app)
+    .post('/users')
+    .send({ email, password })
+    .expect(400)
+    .end(done)
   })
 })
