c8d8e800a7429a863375bff5d585f6eb7606f526
baco16g <baco.16g@gmail.com>
2018-11-11 22:05:18 +0900

    refactor(node-todo-api): change expect methods

--- node-todo-api/server/test/server.test.js
+++ node-todo-api/server/test/server.test.js
@@ -121,7 +121,7 @@ describe('DELETE /todos/:id', () => {
         }
         Todo.findById(hexId)
           .then(todo => {
-            expect(todo).toBeNull()
+            expect(todo).toBeFalsy()
             done()
           })
           .catch(e => done(e))
@@ -140,7 +140,7 @@ describe('DELETE /todos/:id', () => {
         }
         Todo.findById(hexId)
           .then(todo => {
-            expect(todo).not.toBeNull()
+            expect(todo).toBeTruthy()
             done()
           })
           .catch(e => done(e))
@@ -204,7 +204,7 @@ describe('PATCH /todos/:id', () => {
       .expect(res => {
         expect(res.body.todo.text).toBe(text)
         expect(res.body.todo.completed).toBe(false)
-        expect(res.body.todo.completedAt).toBeNull()
+        expect(res.body.todo.completedAt).toBeFalsy()
       })
       .end(done)
   })
@@ -260,8 +260,8 @@ describe('POST /users', () => {
       .send({ email, password })
       .expect(200)
       .expect(res => {
-        expect(res.headers['x-auth']).toBeDefined()
-        expect(res.body._id).toBeDefined()
+        expect(res.headers['x-auth']).toBeTruthy()
+        expect(res.body._id).toBeTruthy()
         expect(res.body.email).toBe(email)
       })
       .end(err => {
@@ -270,7 +270,7 @@ describe('POST /users', () => {
         }
         User.findOne({ email })
           .then(user => {
-            expect(user).toBeDefined()
+            expect(user).toBeTruthy()
             expect(user.password).not.toBe(password)
             done()
           })
@@ -307,7 +307,7 @@ describe('POST /users/login', () => {
       .send({ email, password })
       .expect(200)
       .expect(res => {
-        expect(res.headers['x-auth']).toBeDefined()
+        expect(res.headers['x-auth']).toBeTruthy()
       })
       .end((err, res) => {
         if (err) {
@@ -315,7 +315,7 @@ describe('POST /users/login', () => {
         }
         User.findById(_id)
           .then(user => {
-            expect(user.tokens[1]).toMatchObject({
+            expect(user.toObject().tokens[1]).toMatchObject({
               access: 'auth',
               token: res.headers['x-auth']
             })
@@ -332,7 +332,7 @@ describe('POST /users/login', () => {
       .send({ email, password: `${password}1` })
       .expect(400)
       .expect(res => {
-        expect(res.headers['x-auth']).not.toBeDefined()
+        expect(res.headers['x-auth']).toBeFalsy()
       })
       .end((err, res) => {
         if (err) {
